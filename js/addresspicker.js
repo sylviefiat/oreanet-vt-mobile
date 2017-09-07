86528cd3255905f851f0754a613bd8db16c29010
sylviefiat <sylvie.fiat@ird.fr>
2017-09-08 09:40:20 +1100

    fix(zoom): remove zoom when click on observation geolocalisation

--- js/addresspicker.js
+++ js/addresspicker.js
@@ -369,11 +369,11 @@ if(!bg){
 				cleanData.radius = Math.round(that.gcircle.getRadius()) / 1000;
 			}
 
-			if(that.gcircle){				
+			/*if(that.gcircle){				
 				that.gmap.fitBounds(that.gcircle.getBounds());
 			} else {
 				that.gmap.fitBounds(data.geometry.viewport);
-			}
+			}*/
 
 			data.cleanData = cleanData;
 			updateElements.call(that,data);
